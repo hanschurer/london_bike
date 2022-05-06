441700b46b287f37e7ff8336921aa169a8d1b2a9
Han Wang <hanwangx@gmail.com>
2022-05-06 18:46:41 +0100

    chore: moving mapstyle out of comonent for depoly

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import DirectionsBikeOutlined from "@material-ui/icons/DirectionsBikeOutlined";
-import mapStyles from "./mapStyles";
+import mapStyles from "../../mapStyles";
 import useStyles from "./styles.js";
 
 const Map = ({ coords, stations, setCoords, setBounds, setChildClicked }) => {
